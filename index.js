dd8bd87be26e9efad7b110857743b92ffc211a25
Rene <renebehrens90@gmail.com>
2025-02-10 21:34:54 -0400

    arreglos interfaz y Js "funcional"
    Falta el sistema de puntaje y la parte de sonidos

--- index.js
+++ index.js
@@ -2,42 +2,37 @@ const topLeft = document.querySelector('.top-left-quarter');
 const topRight = document.querySelector('.top-right-quarter');
 const bottomLeft = document.querySelector('.bottom-left-quarter');
 const bottomRight = document.querySelector('.bottom-right-quarter');
-const startButton = document.querySelector('.start-button');
+const startButton = document.querySelector('#start-button');
 
 const getRandomQuarter = () => {
     const quarters = [topLeft, topRight, bottomLeft, bottomRight];
-    return quarters[parseInt(Math.random() * quarters.length)];
+    return quarters[Math.floor(Math.random() * quarters.length)];
 }
 
 const sequences = [getRandomQuarter()];
 let sequenceToGuess = [...sequences];
 
-const flash = (quarter) => {
-    return new Promise((resolve) => {
-        quarter.className += ' active';
-        setTimeout(() => {
-            quarter.className = quarter.className.replace(' active', '');
-            
-            setTimeout(() => {
-                resolve();
-            }, 250);
-
-            resolve();
-        }, 750);
-    });
+const flash = async (quarter) => {
+    quarter.classList.add('active');
+    await new Promise((resolve => setTimeout(resolve,500)));
+    quarter.classList.remove('active');
+    await new Promise((resolve => setTimeout(resolve,250)));
 }
 
 let canClick = false;
 
 const quarterCLicked = (quarter) => {
     if (!canClick) return;
-    console.log(quarter);
     const expectedQuarter = sequenceToGuess.shift();
-    if(expectedQuarter === quarterCLicked){
+    if(expectedQuarter === quarter){
         if(sequenceToGuess.length === 0){
             sequences.push(getRandomQuarter());
             sequenceToGuess = [...sequences];
-            main();
+            canClick = false;
+            setTimeout(() => {
+                main();
+            }, 1000);
+
         }
     }
     else{
